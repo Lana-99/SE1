5672d5a8f7f6b24dd7c4c51c34462797e6645ba2
Svetlana Nikitina <Lanochka99Nikitina@yandex.ru>
2022-07-31 20:42:32 +0500

    10th commit

--- main.cpp
+++ main.cpp
@@ -9,11 +9,12 @@ using namespace std;
 
 int main()
 {
-    int x, y, sum, dif;
+    int x, y, sum, dif, mult;
 
     cout << "Enter X: " << endl; cin >> x;
     cout << "Enter Y: " << endl; cin >> y;
     cout << "Sum: " << x + y << endl;
     cout << "Dif: " << x - y << endl;
+    cout << "Multiplication: " << x * y << endl;
     return 0;
 }
